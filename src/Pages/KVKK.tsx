b55531ae9d0f405928cd8c7d278dc9f5af8a9852
Seyfullah <seyfullah.akgun@coens.io>
2024-08-31 05:51:55 +0300

    son hali

---
+++ src/Pages/KVKK.tsx
@@ -0,0 +1,13 @@
+const KVKK = () => {
+  return (
+    <div>
+      <iframe
+        src="/files/kvkk.pdf"
+        className="w-screen h-screen"
+        title="PDF Viewer"
+      />
+    </div>
+  );
+};
+
+export default KVKK;
---
+++ src/Pages/PrivacyPolicy.tsx
@@ -0,0 +1,13 @@
+const PrivacyPolicy = () => {
+  return (
+    <div>
+      <iframe
+        src="/files/priv.pdf"
+        className="w-screen h-screen"
+        title="PDF Viewer"
+      />
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
